a67813c2aebcaf307f5b70b6ce4920d4ebebba8a
BarnaTB <barnabastb2@gmail.com>
2022-01-07 01:42:50 +0300

    remove STATICFILES_DIRS setting

--- productimporter/settings.py
+++ productimporter/settings.py
@@ -135,19 +135,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
-]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CORS_ORIGIN_WHITELIST = [
-     'http://localhost:3000'
-]
 
 CORS_ORIGIN_WHITELIST = get_env_variable("CORS_ORIGIN_WHITELIST", required=True).split(" ")
 
